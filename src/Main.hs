a825da20cebfe75e5c6193c10ca1a299f1043348
Matheus Drumond <66804348+arg4gg@users.noreply.github.com>
2024-04-03 15:54:06 -0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -5,5 +5,13 @@ import Valor (valorTotal)
 import System.IO (stdout, hSetBuffering, BufferMode(NoBuffering))
 
 main :: IO ()
-main =
-  undefined
+main = do
+  putStrLn "Digite a quantidade de Morangos (em kg): "
+    inputMorangos <- getLine
+    putStrLn "Digite a quantidade de Maçãs (em kg): "
+    inputMacas <- getLine
+    let kgMorangos = read inputMorangos :: Double
+        kgMacas = read inputMacas :: Double
+    if kgMorangos >= 0 && kgMacas >= 0
+        then putStrLn $ "O valor total da compra é R$ " ++ show (valorTotal kgMorangos kgMacas)
+        else putStrLn "Entrada inválida"
