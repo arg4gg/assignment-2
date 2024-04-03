f9995a7456e6fd7ec091b2c1abac0c999624cc16
Matheus Drumond <66804348+arg4gg@users.noreply.github.com>
2024-04-03 15:53:16 -0300

    Update Valor.hs

--- src/Valor.hs
+++ src/Valor.hs
@@ -1,5 +1,8 @@
 module Valor where
 
 valorTotal :: Double -> Double -> Double
-valorTotal morangos maçãs
-  = undefined
+valorTotal kgMorangos kgMacas
+    | kgMorangos <= 5 && kgMacas <= 5 = kgMorangos * 8.5 + kgMacas * 5.25
+    | kgMorangos > 5 && kgMacas > 5 = kgMorangos * 7.2 + kgMacas * 4.75
+    | kgMorangos <= 5 && kgMacas > 5 = kgMorangos * 8.5 + kgMacas * 4.75
+    | kgMorangos  > 5 && kgMacas <= 5 = kgMorangos * 7.2 + kgMacas * 5.25
